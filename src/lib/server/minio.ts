793bacd604fd907b81d720ad6f1514929ae0991f
frittesauce <9026879@student.zadkine.nl>
2025-04-08 15:35:23 +0200

    image uploading

--- src/lib/server/minio.ts
+++ src/lib/server/minio.ts
@@ -1,12 +1,10 @@
 import { env } from '$env/dynamic/private';
 import { Client } from 'minio';
 
-const minioClient = new Client({
-	endPoint: 'localhost',
+export const minioClient = new Client({
+	endPoint: '172.20.0.2',
 	port: 9000,
 	useSSL: false,
 	accessKey: env.MINIO_ROOT_USER,
 	secretKey: env.MINIO_ROOT_PASSWORD
 });
-
-module.exports = minioClient;
--- src/routes/api/profileInit/+server.ts
+++ src/routes/api/profileInit/+server.ts
@@ -3,6 +3,8 @@ import { auth } from '@/auth';
 import { db } from '@/db';
 import { profile, user } from '@/db/schema';
 import { eq } from 'drizzle-orm';
+import { minioClient } from '@/server/minio';
+import sharp from 'sharp';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const session = await auth.api.getSession({
@@ -11,12 +13,14 @@ export const POST: RequestHandler = async ({ request }) => {
 
 	const bucket = 'avatars';
 	const formdata = await request.formData();
-	const file = await formdata.get('image');
-	const username = await formdata.get('username');
+	const file = formdata.get('image');
+	const username = formdata.get('username');
 
 	if (!(file instanceof File) && !(username instanceof String)) {
 		return json({ error: 'file not valid!' }, { status: 400 });
 	}
+
+	console.log(file);
 
 	if (session?.user.finishedOnboard || !session?.user) {
 		return json({ error: 'authentication error!' }, { status: 400 });
@@ -40,7 +44,15 @@ export const POST: RequestHandler = async ({ request }) => {
 		}
 
 		await db.update(user).set({ finishedOnboard: true }).where(eq(user.id, newProfile.userId));
-		const objectName = `${newProfile.id}`;
+		const objectName = `${newProfile.id}/medium.jpg`;
+		const buffer = Buffer.from(await file.arrayBuffer());
+
+		const processedBuffer = await sharp(buffer)
+			.resize(320, 320, { fit: 'cover', position: 'center' })
+			.jpeg({ quality: 80 })
+			.toBuffer();
+
+		await minioClient.putObject(bucket, objectName, processedBuffer);
 
 		return json({ error: 's' });
 	} catch (error) {
