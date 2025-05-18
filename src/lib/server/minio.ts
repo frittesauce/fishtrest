cfe415fd1eb27a31b5cb9b348abc5c00b5eeb40f
frittesauce <9026879@student.tcrmbo.nl>
2025-05-18 13:31:59 +0200

    added deleting

--- src/lib/server/minio.ts
+++ src/lib/server/minio.ts
@@ -2,7 +2,7 @@ import { env } from '$env/dynamic/private';
 import { Client } from 'minio';
 
 export const minioClient = new Client({
-	endPoint: '172.20.0.1',
+	endPoint: '172.18.0.2',
 	port: 9000,
 	useSSL: false,
 	accessKey: env.MINIO_ROOT_USER,
